9db3870c45d3c7d84b273effaa898b1c55b720fd
unknown <satya-1298>
2023-06-14 09:39:21 +0530

    [Satya] Add: First program

---
+++ FirstDemo/Greeting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstDemo
+{
+    public class Greeting
+    {
+        public void Demo()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
